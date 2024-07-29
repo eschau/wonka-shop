9e3e7c7a7b8813846fc4f56f15263ff933de4629
Elizabeth Chau Harris <liz.chau13@gmail.com>
2024-07-28 22:29:57 -0700

    install vite eslint plugin

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), eslint()],
   test: {
     globals: true,
     environment: 'jsdom',
